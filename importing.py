ebc6b7cc0987d386681e78e2dee3e301dbda0d52
Mary Vo <maryvo.uk@gmail.com>
2023-05-13 20:04:47 -0500

    uploading predictive modeling exercise

--- importing.py
+++ importing.py
@@ -2,7 +2,7 @@ from __future__ import division
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt
-from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+from wordcloud import WordCloud
 import seaborn as sns
 
 # import os
